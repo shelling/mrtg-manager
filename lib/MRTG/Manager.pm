5fb59ebbd360129982dab60d1706b78cf89ec024
shelling <navyblueshellingford@gmail.com>
2009-03-08 15:29:48 +0800

    initial import

---
+++ lib/MRTG/Manager.pm
@@ -0,0 +1,51 @@
+package MRTG::Manager;
+
+use 5.008008;
+use strict;
+use warnings;
+
+
+sub new {
+  
+}
+
+
+
+1;
+__END__
+
+=head1 NAME
+
+MRTG::Manager - MRTG Configuration Manager System
+
+=head1 SYNOPSIS
+
+  use MRTG::Manager;
+
+=head1 DESCRIPTION
+
+
+=head2 EXPORT
+
+
+=head1 DEPENDENCE
+
+MRTG 
+
+=head1 SEE ALSO
+
+
+=head1 AUTHOR
+
+shelling, E<lt>shelling@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2009 by shelling
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.8 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
